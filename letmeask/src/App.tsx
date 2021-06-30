78d2cf22ae0fa0db4f512038b3e64e7ac5b175f3
Sergio Damaceno Botelho <sergio.damaceno0002@gmail.com>
2021-06-30 05:09:19 -0300

    NLW 01

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <Button text="Button" />
+      <Button text="2" />
+      <Button text="3" />
+      <Button text="4" />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
